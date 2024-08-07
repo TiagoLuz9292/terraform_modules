f78d9e74d3e2f99ea0d009cef53303d35ce97f45
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-07 18:43:06 +0100

    fix vpc_cidr

--- vpc_peering/variables.tf
+++ vpc_peering/variables.tf
@@ -1,11 +1,35 @@
-variable "vpc_id" {}
-variable "peer_vpc_id" {}
-variable "peer_vpc_cidr" {}
-variable "vpc_cidr" {}
-variable "route_table_id" {}
-variable "admin_route_table_id" {}
-variable "peer_name" {}
-variable "environment_tags" {}
+variable "vpc_id" {
+  description = "The ID of the VPC"
+}
+
+variable "peer_vpc_id" {
+  description = "The ID of the peer VPC"
+}
+
+variable "peer_vpc_cidr" {
+  description = "The CIDR block of the peer VPC"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+}
+
+variable "route_table_id" {
+  description = "The ID of the route table"
+}
+
+variable "admin_route_table_id" {
+  description = "The ID of the admin route table"
+}
+
+variable "peer_name" {
+  description = "The name of the VPC peering connection"
+}
+
+variable "environment_tags" {
+  description = "Environment tags"
+  type        = map(string)
+}
 
 variable "aws_region" {
   description = "AWS region"
