799ea21123092f4c03d2f693e78908fbd598f288
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-07 13:32:37 +0100

    updates

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -12,3 +12,13 @@ output "bucket_domain_name" {
   description = "The domain name of the S3 bucket"
   value       = aws_s3_bucket.this.bucket_domain_name
 }
+
+output "state_bucket_name" {
+  description = "The name of the S3 bucket used for Terraform state"
+  value       = aws_s3_bucket.this.bucket
+}
+
+output "state_table_name" {
+  description = "The name of the DynamoDB table used for Terraform state locks"
+  value       = aws_dynamodb_table.terraform_locks.name
+}
