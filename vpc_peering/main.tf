95ecdec51e90a9968f5daa3be7b3a1a75581cc72
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-08 11:49:51 +0100

    updates

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc_peering_connection" "peer" {
+  provider      = aws.subaccount
   vpc_id        = var.vpc_id
   peer_vpc_id   = var.peer_vpc_id
   auto_accept   = true
-
   tags = merge(
     {
       Name = var.peer_name
@@ -10,4 +10,3 @@ resource "aws_vpc_peering_connection" "peer" {
     var.environment_tags
   )
 }
-
