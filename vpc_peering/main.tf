c0f61f680de886374a109c7402852552847973a9
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-08 11:48:23 +0100

    updates

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -11,14 +11,3 @@ resource "aws_vpc_peering_connection" "peer" {
   )
 }
 
-resource "aws_route" "dev_to_admin" {
-  route_table_id            = var.route_table_id
-  destination_cidr_block    = var.peer_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "admin_to_dev" {
-  route_table_id            = var.admin_route_table_id
-  destination_cidr_block    = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
