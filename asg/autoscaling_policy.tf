7ff1aeae6e39b7c13194f0ab74e805f7421e28b1
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-09 19:33:46 +0100

    updates

--- asg/autoscaling_policy.tf
+++ asg/autoscaling_policy.tf
@@ -1,15 +1,32 @@
-resource "aws_autoscaling_policy" "scale_out_policy" {
+resource "aws_autoscaling_policy" "scale_out_policy_blue" {
   name                   = "scale-out"
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 60
-  autoscaling_group_name = aws_autoscaling_group.asg.name
+  autoscaling_group_name = aws_autoscaling_group.asg_blue[0].name
 }
 
-resource "aws_autoscaling_policy" "scale_in_policy" {
+resource "aws_autoscaling_policy" "scale_out_policy_green" {
+  name                   = "scale-out"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.asg_green[0].name
+}
+
+resource "aws_autoscaling_policy" "scale_in_policy"_blue {
   name                   = "scale-in"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 60
-  autoscaling_group_name = aws_autoscaling_group.asg.name
+  autoscaling_group_name = aws_autoscaling_group.asg_blue[0].name
 }
+
+resource "aws_autoscaling_policy" "scale_in_policy_green" {
+  name                   = "scale-in"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 60
+  autoscaling_group_name = aws_autoscaling_group.asg_green[0].name
+}
+
--- asg/outputs.tf
+++ asg/outputs.tf
@@ -1,19 +1,3 @@
-output "launch_template_id" {
-  description = "The ID of the launch template"
-  value       = aws_launch_template.instance.id
-}
-
-output "autoscaling_group_name" {
-  description = "The name of the Auto Scaling group"
-  value       = aws_autoscaling_group.asg.name
-}
-
-output "scale_out_policy_arn" {
-  description = "The ARN of the scale-out policy"
-  value       = aws_autoscaling_policy.scale_out_policy.arn
-}
-
-output "scale_in_policy_arn" {
-  description = "The ARN of the scale-in policy"
-  value       = aws_autoscaling_policy.scale_in_policy.arn
+output "autoscaling_group_names" {
+  value = var.deployment_strategy == "blue-green" ? [aws_autoscaling_group.asg_blue[0].name, aws_autoscaling_group.asg_green[0].name] : [aws_autoscaling_group.asg_green[0].name]
 }
