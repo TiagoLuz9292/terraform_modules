676f02ef7e673e7117673bbafd7b66d40c540e23
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-09 16:39:45 +0100

    updates

--- asg/autoscaling_group.tf
+++ asg/autoscaling_group.tf
@@ -20,7 +20,14 @@ resource "aws_autoscaling_group" "asg" {
   vpc_zone_identifier  = var.subnet_ids
   target_group_arns    = var.target_group_arns
 
-  tags = var.tags
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 
   mixed_instances_policy {
     instances_distribution {
