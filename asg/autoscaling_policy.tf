a845a5a1bf801193d7331971106edaf781293e4b
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-09 19:35:17 +0100

    updates

--- asg/autoscaling_policy.tf
+++ asg/autoscaling_policy.tf
@@ -14,7 +14,7 @@ resource "aws_autoscaling_policy" "scale_out_policy_green" {
   autoscaling_group_name = aws_autoscaling_group.asg_green[0].name
 }
 
-resource "aws_autoscaling_policy" "scale_in_policy"_blue {
+resource "aws_autoscaling_policy" "scale_in_policy_blue" {
   name                   = "scale-in"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
